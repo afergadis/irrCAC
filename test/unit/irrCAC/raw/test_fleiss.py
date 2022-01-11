0c7676d2a7cc80474285da1f60a4dede9bf02166
Aris Fergadis <aris.fergadis@protonmail.com>
2022-01-11 17:49:03 +0200

    test(raw): test weighted Fleiss' and Gwet's coefficients.

--- test/unit/irrCAC/raw/test_fleiss.py
+++ test/unit/irrCAC/raw/test_fleiss.py
@@ -5,82 +5,177 @@ from irrCAC.datasets import raw_4raters, raw_5observers, raw_g1g2, raw_gender, \
 
 
 class TestFleiss(TestCase):
-
     def test_fleiss_kappa_raw4raters(self):
         data = raw_4raters()
-        cac = CAC(data)
-        results = cac.fleiss()['est']
+        cac = CAC(data, digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.818, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.239, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.76117, 'Wrong '
+            est['coefficient_value'], 0.761, 'Wrong '
             'coefficient value.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), 0.42438,
+            est['confidence_interval'][0], 0.424,
             'Wrong lower value for confidence interval.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 1,
+            est['confidence_interval'][1], 1,
             'Wrong upper value for confidence interval.')
+        self.assertEqual(round(est['p_value'], 5), 0.00042, 'Wrong p value.')
+
+    def test_fleiss_kappa_raw4raters_bipolar_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='bipolar', digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.968, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.785, 'Wrong pe value.')
+        self.assertEqual(est['coefficient_value'], 0.853)
+        self.assertEqual(est['se'], 0.145, 'Wrong standard error.')
         self.assertEqual(
-            round(results['p_value'], 5), 0.00042, 'Wrong p value.')
+            est['confidence_interval'][0], 0.535, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 1e-04, 'Wrong p-value.')
+
+    def test_fleiss_kappa_raw4raters_circular_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='circular', digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.902, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.494, 'Wrong pe value.')
+        self.assertEqual(est['coefficient_value'], 0.807)
+        self.assertEqual(est['se'], 0.149, 'Wrong standard error.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.479, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00021, 'Wrong p-value.')
+
+    def test_fleiss_kappa_raw4raters_linear_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='linear', digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.939, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.667, 'Wrong pe value.')
+        self.assertEqual(est['coefficient_value'], 0.818)
+        self.assertEqual(est['se'], 0.149, 'Wrong standard error.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.491, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00018, 'Wrong p-value.')
+
+    def test_fleiss_kappa_raw4raters_ordinal_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='ordinal', digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.968, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.788, 'Wrong pe value.')
+        self.assertEqual(est['coefficient_value'], 0.850)
+        self.assertEqual(est['se'], 0.147, 'Wrong standard error.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.527, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00012, 'Wrong p-value.')
+
+    def test_fleiss_kappa_raw4raters_quadratic_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='quadratic', digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.975, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.818, 'Wrong pe value.')
+        self.assertEqual(est['coefficient_value'], 0.865)
+        self.assertEqual(est['se'], 0.146, 'Wrong standard error.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.544, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 1e-04, 'Wrong p-value.')
+
+    def test_fleiss_kappa_raw4raters_radical_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='radical', digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.897, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.511, 'Wrong pe value.')
+        self.assertEqual(est['coefficient_value'], 0.790)
+        self.assertEqual(est['se'], 0.150, 'Wrong standard error.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.460, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 2.7e-04, 'Wrong p-value.')
+
+    def test_fleiss_kappa_raw4raters_ratio_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='ratio', digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.954, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.743, 'Wrong pe value.')
+        self.assertEqual(est['coefficient_value'], 0.821)
+        self.assertEqual(est['se'], 0.152, 'Wrong standard error.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.486, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 2.2e-04, 'Wrong p-value.')
 
     def test_fleiss_kappa_raw5observers(self):
         data = raw_5observers()
-        cac = CAC(data)
-        results = cac.fleiss()['est']
+        cac = CAC(data, digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.616, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.291, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.45762,
-            'Wrong coefficient value.')
+            est['coefficient_value'], 0.458, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.120, 'Wrong standard error.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), 0.19928,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], 0.199, 'Wrong CI lower value.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 0.71596,
-            'Wrong upper value for confidence interval.')
-        self.assertEqual(
-            round(results['p_value'], 5), 0.00195, 'Wrong p value.')
+            est['confidence_interval'][1], 0.716, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00195, 'Wrong p value.')
 
     def test_fleiss_kappa_raw_g1g2(self):
         data = raw_g1g2()
-        cac = CAC(data)
-        results = cac.fleiss()['est']
+        cac = CAC(data, digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.744, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.252, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.65725, 'Wrong '
-            'coefficient value.')
+            est['coefficient_value'], 0.657, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.150, 'Wrong standard error.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), 0.33337,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], 0.333, 'Wrong CI lower value.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 0.98112,
-            'Wrong upper value for confidence interval.')
-        self.assertEqual(
-            round(results['p_value'], 5), 0.00074, 'Wrong p value.')
+            est['confidence_interval'][1], 0.981, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00074, 'Wrong p value.')
 
     def test_fleiss_kappa_raw_gender(self):
         data = raw_gender()
-        cac = CAC(data)
-        results = cac.fleiss()['est']
+        cac = CAC(data, digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.456, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.375, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.12889, 'Wrong '
-            'coefficient value.')
+            est['coefficient_value'], 0.129, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.141, 'Wrong standard error.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), -0.17246,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], -0.172, 'Wrong CI lower value.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 0.43023,
-            'Wrong upper value for confidence interval.')
-        self.assertEqual(
-            round(results['p_value'], 5), 0.37449, 'Wrong p value.')
+            est['confidence_interval'][1], 0.430, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.37449, 'Wrong p value.')
 
     def test_fleiss_kappa_raw_ben_gerry(self):
         data = raw_ben_gerry()
-        cac = CAC(data)
-        results = cac.fleiss()['est']
+        cac = CAC(data, digits=3)
+        est = cac.fleiss()['est']
+        self.assertEqual(est['pa'], 0.727, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.222, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.64935, 'Wrong coefficient value.')
+            est['coefficient_value'], 0.649, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.191, 'Wrong standard error.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), 0.22831,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], 0.228, 'Wrong CI lower value.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 1,
-            'Wrong upper value for confidence interval.')
-        self.assertEqual(
-            round(results['p_value'], 5), 0.00599, 'Wrong p value.')
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00599, 'Wrong p value.')
--- test/unit/irrCAC/raw/test_gwet.py
+++ test/unit/irrCAC/raw/test_gwet.py
@@ -1,102 +1,186 @@
 from unittest import TestCase
-from irrCAC.datasets import raw_4raters, raw_5observers, raw_g1g2, raw_gender
+from irrCAC.datasets import raw_4raters, raw_5observers, raw_g1g2, raw_gender, \
+    raw_ben_gerry
 from irrCAC.raw import CAC
 
 
 class TestGwet(TestCase):
-
     def test_gwet_raw4raters(self):
         data = raw_4raters()
-        cac = CAC(data)
-        results = cac.gwet()['est']
+        cac = CAC(data, digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.818, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.190, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.77544, 'Wrong '
-            'coefficient value.')
+            est['coefficient_value'], 0.775, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.143, 'Wrong standard error value.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), 0.46081,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], 0.461, 'Wrong CI lower value.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 1,
-            'Wrong upper value for confidence interval.')
-        self.assertEqual(
-            round(results['p_value'], 5), 0.00021, 'Wrong p value.')
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00021, 'Wrong p-value.')
 
     def test_gwet_raw4raters_bipolar_weights(self):
         data = raw_4raters()
-        cac = CAC(data, weights='bipolar')
-        results = cac.gwet()
-        est = results['est']
+        cac = CAC(data, weights='bipolar', digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.968, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.682, 'Wrong pe value.')
         self.assertEqual(
-            round(est['coefficient_value'], 5), 0.90037, 'Wrong coefficient '
-            'value.')
+            est['coefficient_value'], 0.900, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.106, 'Wrong standard error value.')
         self.assertEqual(
-            round(est['confidence_interval'][0], 5), 0.66747,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], 0.667, 'Wrong CI lower value.')
         self.assertEqual(
-            round(est['confidence_interval'][1], 5), 1,
-            'Wrong upper value for confidence interval.')
-        self.assertEqual(round(est['p_value'], 5), 0.0, 'Wrong p value.')
+            est['confidence_interval'][1], 1, 'Wrong CI upper value')
+        self.assertEqual(round(est['p_value'], 6), 4e-6, 'Wrong p value.')
+
+    def test_gwet_raw4raters_circular_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='circular', digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.902, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.426, 'Wrong pe value.')
+        self.assertEqual(
+            est['coefficient_value'], 0.830, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.133, 'Wrong standard error value.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.538, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value')
+        self.assertEqual(round(est['p_value'], 5), 6e-5, 'Wrong p value.')
+
+    def test_gwet_raw4raters_linear_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='linear', digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.939, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.571, 'Wrong pe value.')
+        self.assertEqual(
+            est['coefficient_value'], 0.859, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.117, 'Wrong standard error value.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.600, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value')
+        self.assertEqual(round(est['p_value'], 5), 2e-5, 'Wrong p value.')
+
+    def test_gwet_raw4raters_ordinal_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='ordinal', digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.968, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.685, 'Wrong pe value.')
+        self.assertEqual(
+            est['coefficient_value'], 0.899, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.107, 'Wrong standard error value.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.664, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value')
+        self.assertEqual(round(est['p_value'], 6), 4e-6, 'Wrong p value.')
 
     def test_gwet_raw4raters_quadratic_weights(self):
         data = raw_4raters()
-        cac = CAC(data, weights='quadratic')
-        results = cac.gwet()['est']
+        cac = CAC(data, weights='quadratic', digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.975, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.714, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.914, 'Wrong coefficient '
-            'value.')
+            est['coefficient_value'], 0.914, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.104, 'Wrong standard error value.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), 0.68518,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], 0.685, 'Wrong CI lower value.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 1,
-            'Wrong upper value for confidence interval.')
-        self.assertEqual(round(results['p_value'], 5), 0.0, 'Wrong p value.')
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 6), 3e-06, 'Wrong p value.')
+
+    def test_gwet_raw4raters_radical_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='radical', digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.897, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.430, 'Wrong pe value.')
+        self.assertEqual(
+            est['coefficient_value'], 0.820, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.128, 'Wrong standard error value.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.537, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 5e-05, 'Wrong p value.')
+
+    def test_gwet_raw4raters_ratio_weights(self):
+        data = raw_4raters()
+        cac = CAC(data, weights='ratio', digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.954, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.678, 'Wrong pe value.')
+        self.assertEqual(
+            est['coefficient_value'], 0.857, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.122, 'Wrong standard error value.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.589, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 2e-05, 'Wrong p value.')
 
     def test_gwet_raw5observers(self):
         data = raw_5observers()
-        cac = CAC(data)
-        results = cac.gwet()['est']
+        cac = CAC(data, digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.616, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.236, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.49662, 'Wrong '
-            'coefficient value.')
+            est['coefficient_value'], 0.497, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.122, 'Wrong standard error value.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), 0.23502,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], 0.235, 'Wrong CI lower value.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 0.75823,
-            'Wrong upper value for confidence interval.')
-        self.assertEqual(
-            round(results['p_value'], 5), 0.00114, 'Wrong p value.')
+            est['confidence_interval'][1], 0.758, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00114, 'Wrong p value.')
 
     def test_gwet_raw_g1g2(self):
         data = raw_g1g2()
-        cac = CAC(data)
-        results = cac.gwet()['est']
+        cac = CAC(data, digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.744, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.187, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.6846,
-            'Wrong coefficient '
-            'value.')
+            est['coefficient_value'], 0.685, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.134, 'Wrong standard error value.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), 0.39612,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], 0.396, 'Wrong CI lower value.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 0.97308,
-            'Wrong upper value for confidence interval.')
-        self.assertEqual(
-            round(results['p_value'], 5), 0.00019, 'Wrong p value.')
+            est['confidence_interval'][1], 0.973, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00019, 'Wrong p value.')
 
     def test_gwet_raw_gender(self):
         data = raw_gender()
-        cac = CAC(data)
-        results = cac.gwet()['est']
+        cac = CAC(data, digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.456, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.312, 'Wrong pe value.')
         self.assertEqual(
-            round(results['coefficient_value'], 5), 0.20808, 'Wrong '
-            'coefficient value.')
+            est['coefficient_value'], 0.208, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.091, 'Wrong standard error value.')
         self.assertEqual(
-            round(results['confidence_interval'][0], 5), 0.01356,
-            'Wrong lower value for confidence interval.')
+            est['confidence_interval'][0], 0.014, 'Wrong CI lower value.')
         self.assertEqual(
-            round(results['confidence_interval'][1], 5), 0.40261,
-            'Wrong upper value for confidence interval.')
+            est['confidence_interval'][1], 0.403, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.03776, 'Wrong p value.')
+
+    def test_gwet_raw_ben_gerry(self):
+        data = raw_ben_gerry()
+        cac = CAC(data, digits=3)
+        est = cac.gwet()['est']
+        self.assertEqual(est['pa'], 0.727, 'Wrong pa value.')
+        self.assertEqual(est['pe'], 0.194, 'Wrong pe value.')
         self.assertEqual(
-            round(results['p_value'], 5), 0.03776, 'Wrong p value.')
+            est['coefficient_value'], 0.661, 'Wrong coefficient value.')
+        self.assertEqual(est['se'], 0.184, 'Wrong standard error value.')
+        self.assertEqual(
+            est['confidence_interval'][0], 0.257, 'Wrong CI lower value.')
+        self.assertEqual(
+            est['confidence_interval'][1], 1, 'Wrong CI upper value.')
+        self.assertEqual(round(est['p_value'], 5), 0.00420, 'Wrong p value.')
